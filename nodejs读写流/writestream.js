f5677d06bb76545ed9efd158560ffd2543aa00fe
animate <31640196+lala19@users.noreply.github.com>
2018-07-20 11:54:59 +0800

    Update writestream.js

--- nodejs读写流/writestream.js
+++ nodejs读写流/writestream.js
@@ -1,11 +1,11 @@
 var fs = require("fs");
-var data = '菜鸟教程官网地址：www.runoob.com';
+var data = 'asssssssssas';
 
 //创建一个可以写入的流，写入到文件 output.txt中
 var writerStream = fs.createWriteStream('output.txt');
 //写
 writerStream.write(data, 'UTF8');
 //监听写完
-writerStream.end('finish', function() {
+writerStream.end(function() {
 	console.log("写入完成。");
 });
